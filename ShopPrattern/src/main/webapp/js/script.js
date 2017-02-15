e78c1cf214da211bbfc1c7a81a5eb80a8082b872
danul <danul@DESKTOP-0MKJDAG>
2017-02-15 19:31:15 +0200

    fix

--- ShopPrattern/src/main/webapp/js/script.js
+++ ShopPrattern/src/main/webapp/js/script.js
@@ -1,15 +1,3 @@
-$("#header nav a").click(function(e) {
-	e.preventDefault();		
-	$("#header nav a").removeClass("active");
-	$(this).addClass("active");
-});
-$("#city").click(function(e) {
-	document.getElementById("wrapper-city-menu").style="display:block;position:absolute";
-});
-$("#close").click(function(e) {
-	document.getElementById("wrapper-city-menu").style="display:none;";
-});
-
 function action(){
 	
 	$("#header nav a").click(function(e) {
